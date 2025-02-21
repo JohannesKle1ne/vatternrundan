77a98c3bf49a896ba1565497f8e2fe487b8d005e
JohannesKle1ne <jojo.kleine@gmx.de>
2025-02-21 11:45:16 +0100

    added csv saveing

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs");
+const path = require("path");
+const { parse } = require("json2csv");
 
-function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-function clickWithDelay(button) {
-  return new Promise((resolve) => {
-    button.click();
-    setTimeout(resolve, 2000);
-  });
-}
-
-const checkSite = async () => {
-  console.log("Launch pupeteer..");
+async function checkSite() {
+  console.log("Launching Puppeteer...");
 
   const browser = await puppeteer.launch({
     args: ["--no-sandbox"],
@@ -22,148 +14,122 @@ const checkSite = async () => {
   await page.goto(
     "https://www.sport-conrad.com/ski-alpin/ski/freeride-freetouring-ski/?count=300&offset=0"
   );
-  console.log("Visiting page..");
+  console.log("Visiting page...");
+
   await page.waitForSelector(".sc-product-list-tile__action--to-product", {
     visible: true,
   });
+  console.log("Found 'to product' button");
 
-  console.log("Found to product button");
+  const skiData = [];
 
   for (let j = 1; j <= 100; j++) {
     for (let i = 1; i <= 3; i++) {
-      await page.waitForSelector(`.sc-product-list__wrapper > ul`, {
-        visible: true,
-      });
-      console.log("found ul");
-      /*     const selector = `ul.sc-product-list__tiles > li:nth-child(${i}) button.sc-product-list-tile__action--to-product`;
-       */
+      console.log(`row: ${j} item: ${i}`);
+      await page
+        .waitForSelector(".sc-product-list__wrapper > ul", {
+          visible: true,
+        })
+        .catch(() => {
+          console.log("Selector not found within 10 seconds");
+        });
+      console.log("Found product list");
+
       const selector = `.sc-product-list__wrapper > ul > ul.sc-product-list-tile-row:nth-child(${j}) > li:nth-child(${i}) button.sc-product-list-tile__action--to-product`;
       const element = await page.$(selector);
-      console.log("found li element", element);
 
-      if (element == null) continue;
+      if (!element) continue;
+
       await Promise.all([
-        await page.$eval(selector, (element) => {
-          element.click();
-        }),
+        page.$eval(selector, (el) => el.click()),
         page.waitForNavigation(),
       ]);
-      /*   await page.click(".sc-product-list-tile__action--to-product");
-       */
 
-      console.log("look for page meta");
+      console.log("Extracting ski data...");
       await page.waitForSelector(".sc-skiMeta");
-      console.log("found page meta");
 
-      //  await delay(1000);
-
-      const skiObject = {};
-      const name = await page.evaluate(() => {
-        return document
-          .querySelector(".sc-product-buy-box__title")
-          .innerText.trim();
-      });
-      skiObject.name = name;
-
-      const skiWidth = await page.evaluate(() => {
-        const element = document.querySelector(
-          ".sc-skiMeta_attribute-silhouette > span"
-        );
-        return element
-          ? parseInt(element.innerText.replace(/\D/g, ""), 10)
-          : null;
-      });
-      skiObject.skiWidth = skiWidth;
-
-      const radius = await page.evaluate(() => {
-        const element = document.querySelector(
-          ".sc-skiMeta_attribute--radius .sc-skiMeta_attribute--value"
-        );
-        return element
-          ? parseInt(element.innerText.replace(/\D/g, ""), 10)
-          : null;
-      });
-      skiObject.radius = radius;
-
-      const sidecut = await page.evaluate(() => {
-        const element = document.querySelector(
-          ".sc-skiMeta_attribute--sidecut .sc-skiMeta_attribute--value"
-        );
-        return element ? element.innerText.match(/\d+/g).map(Number) : null;
-      });
-      skiObject.sidecut = sidecut.join(", ");
-
-      const weight = await page.evaluate(() => {
-        const rows = document.querySelectorAll(
-          ".sc-product-detail-secondary__attributes--row"
-        );
-        for (let row of rows) {
-          const keyElement = row.querySelector(
-            ".sc-product-detail-secondary__attributes--key"
+      const skiObject = {
+        name: await page.$eval(".sc-product-buy-box__title", (el) =>
+          el.innerText.trim()
+        ),
+        skiWidth: await page.$eval(
+          ".sc-skiMeta_attribute-silhouette > span",
+          (el) => parseInt(el.innerText.replace(/\D/g, ""), 10)
+        ),
+        radius: await page.$eval(
+          ".sc-skiMeta_attribute--radius .sc-skiMeta_attribute--value",
+          (el) => parseInt(el.innerText.replace(/\D/g, ""), 10)
+        ),
+        sidecut: await page.$eval(
+          ".sc-skiMeta_attribute--sidecut .sc-skiMeta_attribute--value",
+          (el) => el.innerText.match(/\d+/g)?.map(Number).join(", ")
+        ),
+        weight: await page.evaluate(() => {
+          const rows = document.querySelectorAll(
+            ".sc-product-detail-secondary__attributes--row"
           );
-          if (
-            keyElement &&
-            keyElement.innerText.trim().toLowerCase() === "gewicht:"
-          ) {
-            const valueElement = row.querySelector(
-              ".sc-product-detail-secondary__attributes--value span"
-            );
-            return valueElement
-              ? parseInt(valueElement.innerText.replace(/\D/g, ""), 10)
-              : null;
+          for (let row of rows) {
+            if (
+              row
+                .querySelector(".sc-product-detail-secondary__attributes--key")
+                ?.innerText.trim()
+                .toLowerCase() === "gewicht:"
+            ) {
+              return parseInt(
+                row
+                  .querySelector(
+                    ".sc-product-detail-secondary__attributes--value span"
+                  )
+                  ?.innerText.replace(/\D/g, ""),
+                10
+              );
+            }
           }
-        }
-        return null;
-      });
-      skiObject.weight = weight;
+          return null;
+        }),
+        sizes: await page.$$eval(
+          ".sc-product-buy-box__option-menu__option",
+          (els) =>
+            els
+              .map((el) => parseInt(el.textContent.trim(), 10))
+              .filter((num) => !isNaN(num))
+              .join(", ")
+        ),
+        price: await page.$eval(".sc-product-buy-box__price--current", (el) =>
+          el.textContent
+            .trim()
+            .replace(/[^\d,]/g, "")
+            .replace(",", ".")
+        ),
+        url: await page.url(),
+      };
 
-      const numbers = await page.evaluate(() => {
-        return Array.from(
-          document.querySelectorAll(".sc-product-buy-box__option-menu__option")
-        )
-          .map((el) => parseInt(el.textContent.trim(), 10))
-          .filter((num) => !isNaN(num));
-      });
-      skiObject.sizes = numbers.join(", ");
+      skiData.push(skiObject);
+      console.log("Added ski object:", skiObject);
 
-      const price = await page.evaluate(() => {
-        const priceElement = document.querySelector(
-          ".sc-product-buy-box__price--current"
-        );
-        return priceElement
-          ? priceElement.textContent
-              .trim()
-              .replace(/[^\d,]/g, "")
-              .replace(",", ".")
-          : null;
-      });
-      skiObject.price = price;
-
-      const currentUrl = await page.url();
-      skiObject.url = currentUrl;
-
-      console.log(skiObject);
       await Promise.all([page.goBack(), page.waitForNavigation()]);
     }
   }
 
-  /*  const lines = innerText.split("\n");
+  await browser.close();
 
-  const weekendDays = ["Freitag", "Samstag", "Sonntag"];
-
-  const weekendLines = lines.filter((l) =>
-    weekendDays.some((d) => l.includes(d))
-  );
-  const hasWeekend = weekendLines.length > 0;
-
-  console.log(hasWeekend);
- */
-
-  browser.close();
-};
+  // Save data to CSV
+  console.log(skiData);
+  const csvFilePath = path.join(__dirname, "ski_data.csv");
+  const csv = parse(skiData, {
+    fields: [
+      { label: "Name", value: "name" },
+      { label: "Mittelbreite", value: "skiWidth" },
+      { label: "Radius", value: "radius" },
+      { label: "Taillierung", value: "sidecut" },
+      { label: "Gewicht", value: "weight" },
+      { label: "Größen", value: "sizes" },
+      { label: "Preis", value: "price" },
+      { label: "Link", value: "url" },
+    ],
+  });
+  fs.writeFileSync(csvFilePath, csv);
+  console.log(`Saved CSV file: ${csvFilePath}`);
+}
 
 checkSite();
-
-setInterval(() => {
-  checkSite();
-}, 1000 * 60 * 5);
